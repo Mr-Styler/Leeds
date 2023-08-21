b69fc49f4edf3768cd7d900ab031ee6ab97ca632
Mr Styler <fortunedeekor@gmail.com>
2023-08-21 11:53:01 +0100

    logout now possible for users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,8 +11,8 @@ router.get('/logout', authController.logout);
 
 
 // Routes that can only be accessed when logged in
+router.get('/me', authController.isAuthenticated, authController.getMe);
 router.use(authController.isAuthenticated, authController.restrictTo(['admin']));
-router.get('/me', authController.getMe);
 
 router.get('/', userController.getAllUsers);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
